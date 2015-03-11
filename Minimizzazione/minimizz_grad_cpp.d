13a152b5b2ec457a275d855f64b7d99652a9facd
Federico Massa <fedemassa91@gmail.com>
2015-03-11 18:07:55 +0100

    Inserito minimizzazione con gradiente..funziona bene ma lento. Da provare raggio variabile

---
+++ Minimizzazione/minimizz_grad_cpp.d
@@ -0,0 +1,46 @@
+
+# DO NOT DELETE
+
+./minimizz_grad_cpp.so: /home/federico/Scaricati/root/include/TMath.h
+./minimizz_grad_cpp.so: /home/federico/Scaricati/root/include/Rtypes.h
+./minimizz_grad_cpp.so: /home/federico/Scaricati/root/include/RConfig.h
+./minimizz_grad_cpp.so: /home/federico/Scaricati/root/include/RVersion.h
+./minimizz_grad_cpp.so: /home/federico/Scaricati/root/include/DllImport.h
+./minimizz_grad_cpp.so: /home/federico/Scaricati/root/include/Rtypeinfo.h
+./minimizz_grad_cpp.so: /home/federico/Scaricati/root/include/snprintf.h
+./minimizz_grad_cpp.so: /home/federico/Scaricati/root/include/strlcpy.h
+./minimizz_grad_cpp.so: /home/federico/Scaricati/root/include/TGenericClassInfo.h
+./minimizz_grad_cpp.so: /home/federico/Scaricati/root/include/TSchemaHelper.h
+./minimizz_grad_cpp.so: /home/federico/Scaricati/root/include/TMathBase.h
+./minimizz_grad_cpp.so: /home/federico/Scaricati/root/include/TError.h
+./minimizz_grad_cpp.so: /home/federico/Scaricati/root/include/TRandom3.h
+./minimizz_grad_cpp.so: /home/federico/Scaricati/root/include/TRandom.h
+./minimizz_grad_cpp.so: /home/federico/Scaricati/root/include/TNamed.h
+./minimizz_grad_cpp.so: /home/federico/Scaricati/root/include/TObject.h
+./minimizz_grad_cpp.so: /home/federico/Scaricati/root/include/TStorage.h
+./minimizz_grad_cpp.so: /home/federico/Scaricati/root/include/TVersionCheck.h
+./minimizz_grad_cpp.so: /home/federico/Scaricati/root/include/Riosfwd.h
+./minimizz_grad_cpp.so: /home/federico/Scaricati/root/include/TBuffer.h
+./minimizz_grad_cpp.so: /home/federico/Scaricati/root/include/TString.h
+./minimizz_grad_cpp.so: /home/federico/Scaricati/root/include/TH2F.h
+./minimizz_grad_cpp.so: /home/federico/Scaricati/root/include/TH2.h
+./minimizz_grad_cpp.so: /home/federico/Scaricati/root/include/TH1.h
+./minimizz_grad_cpp.so: /home/federico/Scaricati/root/include/TAxis.h
+./minimizz_grad_cpp.so: /home/federico/Scaricati/root/include/TAttAxis.h
+./minimizz_grad_cpp.so: /home/federico/Scaricati/root/include/TArrayD.h
+./minimizz_grad_cpp.so: /home/federico/Scaricati/root/include/TArray.h
+./minimizz_grad_cpp.so: /home/federico/Scaricati/root/include/TAttLine.h
+./minimizz_grad_cpp.so: /home/federico/Scaricati/root/include/TAttFill.h
+./minimizz_grad_cpp.so: /home/federico/Scaricati/root/include/TAttMarker.h
+./minimizz_grad_cpp.so: /home/federico/Scaricati/root/include/TArrayC.h
+./minimizz_grad_cpp.so: /home/federico/Scaricati/root/include/TArrayS.h
+./minimizz_grad_cpp.so: /home/federico/Scaricati/root/include/TArrayI.h
+./minimizz_grad_cpp.so: /home/federico/Scaricati/root/include/TArrayF.h
+./minimizz_grad_cpp.so: /home/federico/Scaricati/root/include/Foption.h
+./minimizz_grad_cpp.so: /home/federico/Scaricati/root/include/TVectorFfwd.h
+./minimizz_grad_cpp.so: /home/federico/Scaricati/root/include/TVectorDfwd.h
+./minimizz_grad_cpp.so: /home/federico/Scaricati/root/include/TFitResultPtr.h
+./minimizz_grad_cpp.so: /home/federico/Scaricati/root/include/TMatrixFBasefwd.h
+./minimizz_grad_cpp.so: /home/federico/Scaricati/root/include/TMatrixDBasefwd.h
+./minimizz_grad_cpp.so: /home/federico/Scaricati/root/include/cintdictversion.h /home/federico/Scaricati/root/include/RVersion.h
+minimizz_grad_cpp__ROOTBUILDVERSION= 5.34/11
