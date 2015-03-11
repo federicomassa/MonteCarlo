6958a46aeef1fa764f9393630b7cab5a7634d751
Federico Massa <fedemassa91@gmail.com>
2015-03-11 20:49:31 +0100

    Migliorato un po' la tecnica del gradient descent, manca prop[i] eventualmente stimato per ogni iterazione

--- Minimizzazione/minimizz_grad.cpp
+++ Minimizzazione/minimizz_grad.cpp
@@ -34,6 +34,9 @@ double min_func(int nmeas, double x[], double y[],double sigma, double pars[]) {
 
 
 void minimizz() {
+  double precision = 1E-7;
+  double prop = 1E-5;
+  int first = 0;
   bool sign;
   double norm_grad;
   int npars = 2;
@@ -42,9 +45,9 @@ void minimizz() {
   double* pars = new double[npars];
   double m_t = 6;
   double a_t = -4;
-  double pars_best[npars];
   double pars_step[npars];  //per calcolo derivata
-  double radius = 0.01;
+  double init_radius = 0.001;
+  double radius;
   double pars_init[npars];
   double pars_start[npars];
   double chi2_up[npars];
@@ -63,31 +66,22 @@ void minimizz() {
     y[i] = m_t*x[i]+a_t*x[i]*x[i];
   }
 
-  //grafico chi2
-  TH2F* chi2_hist = new TH2F("chi2_hist","Grafico Chi2;m;a;Chi2",100,5.999,6.005,100,-4.001,-3.999);
-  for (int i = 1; i <= 100; i++) {
-    for (int j = 1; j <= 100; j++) {
-      pars[0] = 5.999 + double(i)/100*0.006;
-      //pars[0] = 6;
-      pars[1] = -4.001 + double(j)/100*0.002;
-	    // pars[1] = -4;
-      chi2_hist->SetBinContent(i,j,min_func(nmeas,x,y,sigma,pars));
-    }
-  }
 
-  chi2_hist->Draw();
 
 
   //inizializzazione parametri
   for (int i = 0; i < npars; i++) {
-    pars_step[i] = 0.00001;
+    pars_step[i] = 0.0001;
     pars_init[0] = 8;
     pars_init[1] = -6;
     pars[i] = pars_init[i];
     chi2 = min_func(nmeas,x,y,sigma,pars);
   }
 
+  first = 0;
+
   do {
+    first++;
     end_cycle = true;
     for (int i = 0; i < npars; i++) {
       pars_start[i] = pars[i];}
@@ -105,14 +99,25 @@ void minimizz() {
       
     }
 
+
     norm_grad = 0;
     for (int i = 0; i < npars; i++)
       norm_grad += grad_up[i]*grad_up[i];
 
-    for (int i = 0; i < npars; i++)
-      grad_up[i] = grad_up[i]/norm_grad*radius;
+        norm_grad = TMath::Sqrt(norm_grad);
+
+    // if (first == 1) {
+    //   prop = 0.0005;
+    // }
+
+
+    //      for (int i = 0; i < npars; i++) pars_step[i] = radius;
+
+	    //    std::cout << prop << '\n' << first << '\n' << radius << '\n' << pars[0] << '\n' << pars[1] << '\n' << std::endl;
     
+
     
+  
 
   sign = true;
   for (int i = 0; i < npars; i++) {
@@ -120,17 +125,18 @@ void minimizz() {
 
   if (sign) { //siamo nel minimo
     end_cycle = true;
-    for (int i = 0; i < npars; i++) {
-      pars_best[i] = pars_start[i];
-    }
     break;
   }
 
   else { 
+    sign = true;
     end_cycle = false;
     for (int i = 0; i < npars; i++) {
-      pars[i] = pars_start[i] - grad_up[i]; // segno - per andare verso il minimo
+      pars[i] = pars_start[i] - prop*grad_up[i]; // segno - per andare verso il minimo
+    
+      if (TMath::Abs(grad_up[i]*prop) > precision) sign = false;
     }
+    end_cycle = sign;
   }
 
  
@@ -143,7 +149,8 @@ void minimizz() {
 
   while (!end_cycle);
   
-  std::cout << "m minimo: " << pars_best[0] << std::endl;
-  std::cout << "a minimo: " << pars_best[1] << std::endl;
+  std::cout << "m minimo: " << pars[0] << std::endl;
+  std::cout << "a minimo: " << pars[1] << std::endl;
   std::cout << "chi2 minimo: " << chi2 << std::endl;
+  std::cout << "iterazioni: " << first << std::endl;
 }
